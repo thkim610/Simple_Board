04fba72c932108998ab93b3952d66c77e7544c0a
thkim610 <thkim610@naver.com>
2024-04-19 16:32:29 +0900

    feat: 게시글 엔터티, 레포지토리 생성

---
+++ src/main/java/com/example/simpleboard/post/db/PostEntity.java
@@ -0,0 +1,29 @@
+package com.example.simpleboard.post.db;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity(name = "post")
+public class PostEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String email;
+    private String status;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime postedAt;
+}
