e9200b0b905e2ad1a261f7de042d0879714197ef
thkim610 <thkim610@naver.com>
2024-04-19 16:20:24 +0900

    feat: 게시판 엔터티 생성

---
+++ src/main/java/com/example/simpleboard/board/db/BoardEntity.java
@@ -0,0 +1,24 @@
+package com.example.simpleboard.board.db;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity(name = "board")
+public class BoardEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String boardName;
+    private String status;
+}
