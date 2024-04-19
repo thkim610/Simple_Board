6d72fb63cc8cc72010dc4a648db47f58f093d571
thkim610 <thkim610@naver.com>
2024-04-19 16:32:36 +0900

    feat: 댓글 엔터티, 레포지토리 생성

---
+++ src/main/java/com/example/simpleboard/reply/db/ReplyEntity.java
@@ -0,0 +1,29 @@
+package com.example.simpleboard.reply.db;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity(name = "reply")
+public class ReplyEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+    private String password;
+    private String status;
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+    private LocalDateTime repliedAt;
+}
